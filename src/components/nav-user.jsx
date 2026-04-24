6f3d053b1c8a594997c84596009d9aa924e0b444
Jayasurya M <jayasurya.m9916@gmail.com>
2026-04-24 18:14:03 +0530

    added return order and vender filter in all create modules

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -100,7 +100,7 @@ export function NavUser({ user }) {
                       </span>
                     </span>
                     <span className="flex items-center gap-1 font-semibold">
-                      Updated on :18/03/2026
+                      Updated on :24/04/2026
                     </span>
                   </div>
                 </div>
